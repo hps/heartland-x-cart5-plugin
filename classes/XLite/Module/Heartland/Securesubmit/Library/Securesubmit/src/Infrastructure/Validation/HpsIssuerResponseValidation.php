83588a129692001abb87620b2b337138147a0d53
Mark Hagan <markhagan@gmail.com>
2016-01-19 14:26:44 -0500

    added advanced fraud lookups to SDK

--- classes/XLite/Module/Heartland/Securesubmit/Library/Securesubmit/src/Infrastructure/Validation/HpsIssuerResponseValidation.php
+++ classes/XLite/Module/Heartland/Securesubmit/Library/Securesubmit/src/Infrastructure/Validation/HpsIssuerResponseValidation.php
@@ -40,6 +40,7 @@ class HpsIssuerResponseValidation
         '91' => HpsExceptionCodes::ISSUER_TIMEOUT,
         'EB' => HpsExceptionCodes::INCORRECT_CVC,
         'N7' => HpsExceptionCodes::INCORRECT_CVC,
+        'FR' => HpsExceptionCodes::POSSIBLE_FRAUD_DETECTED,
     );
 
     public static $_creditExceptionCodeToMessage = array(
@@ -55,6 +56,7 @@ class HpsIssuerResponseValidation
         HpsExceptionCodes::ISSUER_TIMEOUT       => "The card issuer timed-out.",
         HpsExceptionCodes::UNKNOWN_CREDIT_ERROR => "An unknown issuer error has occurred.",
         HpsExceptionCodes::INCORRECT_NUMBER     => "The card number is incorrect."
+        HpsExceptionCodes::POSSIBLE_FRAUD_DETECTED => "Possible fraud detected.",
     );
 
     public static function checkResponse($transactionId, $responseCode, $responseText)
